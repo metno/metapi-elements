30e3ca7842535af0428cdac093e3165354597b2e
Jo Asplin <jo.asplin@met.no>
2017-07-03 12:48:11 +0200

    Added missing element

---
+++ conf/evolutions/elements/39.sql
@@ -0,0 +1,7 @@
+# --- !Ups
+
+-- insert missing element
+INSERT INTO element_codetable (element_id, codetable_name) VALUES ('over_time(precipitation_type_primary_significance P1D)', 'precipitation_type_significance');
+
+
+# --- !Downs
